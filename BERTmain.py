d78c313156c485e707a0ae42ca5b9c39390f8e44
aurora314156 <aurora314156@gmail.com>
2019-02-26 23:41:55 -0800

    getEncodeContent test

--- BERTmain.py
+++ BERTmain.py
@@ -12,15 +12,20 @@ def getEncodeContent(single_data):
         s_string += story_element + " "
     for question_element in single_data['question']:
         q_string += question_element + " "
-    for options in single_data['options']:
+    for option in single_data['options']:
         o_string = ""
-        for single_option_element in options:
+        for single_option_element in option:
             o_string += single_option_element + " "
         options.append(o_string)
-    print(options)
     
+    story = bc.encode([s_string])
+    question = bc.encode([q_string])
+    options = bc.encode(options)
+    print("=================")
+    print(options.shape)
+    print("=================")
     answer = single_data['answer']
-    story,question = "", ""
+
     return story, question, options, answer
 
 def main():
