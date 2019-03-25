e205fdc55c0b4f186694628e4bfd45dacb23ffd6
Ethan <aurora314156@gmail.com>
2019-03-24 23:58:19 -0700

    test original thirdmodel performance

--- BERTService/Initial.py
+++ BERTService/Initial.py
@@ -6,7 +6,7 @@ class Initial():
     def __init__(self):
         self.d_model = 512
         # choose run model
-        self.model = ['ThirdModelWithSoftmax','TenthModel']
+        self.model = ['ThirdModel','ThirdModelWithSoftmax','TenthModel']
     def InitialMain(self):
         self.createLogFile()
         dataset, dataType = self.GetDataset(self.ArgParse())
