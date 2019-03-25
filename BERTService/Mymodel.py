70755209e76877ff0247cdfcc0d6c15fe558760c
Ethan <aurora314156@gmail.com>
2019-03-25 04:24:40 -0700

    bug

--- BERTService/Mymodel.py
+++ BERTService/Mymodel.py
@@ -130,7 +130,7 @@ class Mymodel():
         merStoryQue = [x + y for x, y in zip(story, question)]
         for i in range(20):
             tmp = [x + y for x, y in zip(story, merStoryQue)]
-            merStoryQue = self.relu(np.assary(tmp))
+            merStoryQue = self.relu(np.asarray(tmp))
 
         for option in options:
             tmpScore = 1 - spatial.distance.cosine(merStoryQue, option)
@@ -289,9 +289,39 @@ class Mymodel():
         return guessAnswer
 
 
-    def softmax(self, x):
-        """Compute softmax values for each sets of scores in x."""
-        return np.exp(x) / np.sum(np.exp(x), axis=0)
+    def softmax(x):
+        """
+        Compute the softmax function for each row of the input x.
+
+        Arguments:
+        x -- A N dimensional vector or M x N dimensional numpy matrix.
+
+        Return:
+        x -- You are allowed to modify x in-place
+        """
+        orig_shape = x.shape
+
+        if len(x.shape) > 1:
+            # Matrix
+            exp_minmax = lambda x: np.exp(x - np.max(x))
+            denom = lambda x: 1.0 / np.sum(x)
+            x = np.apply_along_axis(exp_minmax,1,x)
+            denominator = np.apply_along_axis(denom,1,x) 
+            
+            if len(denominator.shape) == 1:
+                denominator = denominator.reshape((denominator.shape[0],1))
+            
+            x = x * denominator
+        else:
+            # Vector
+            x_max = np.max(x)
+            x = x - x_max
+            numerator = np.exp(x)
+            denominator =  1.0 / np.sum(numerator)
+            x = numerator.dot(denominator)
+
+        assert x.shape == orig_shape
+        return x
 
     def relu(self, x):
         return x * (x > 0)
