4a3dd98ae3e3fe253631a2eb9b4c20e0040ef58c
aurora314156 <aurora314156@gmail.com>
2019-03-26 01:40:13 -0700

    test word sequence influence

--- BERTService/Mymodel.py
+++ BERTService/Mymodel.py
@@ -271,9 +271,9 @@ class Mymodel():
         return guessAnswer
 
     def EleventhModel(self, bc):
-        merStoryQue = self.softmax(bc.encode([self.s_string + self.q_string]))
+        merStoryQue = self.softmax(bc.encode([self.q_string + self.s_string]))
         for i in range(len(self.options)):
-            self.options[i] = self.q_string + self.options[i]
+            self.options[i] = self.options[i] + self.q_string 
         
         merQueOpts = self.softmax(bc.encode(self.options))
 
