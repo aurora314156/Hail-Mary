cc160d5e11ae8ffa15022ba1ba4ee734216bb1dc
Ethan <aurora314156@gmail.com>
2019-03-28 06:59:24 -0700

    bug

--- BERTService/SaveLog.py
+++ BERTService/SaveLog.py
@@ -43,11 +43,11 @@ class SaveLog():
         for a in self.AccuracyList:
             sheet.write(ind_y, ind_x, str(a))
             ind_x += 1
-            if ind_x % 9 == 0:
+            if ind_x % 8 == 0:
                 ind_y += 1
                 ind_x = 1
                 continue
-            if ind_x % 4 == 0:
+            elif ind_x % 4 == 0:
                 ind_x +=2
                 continue
             
