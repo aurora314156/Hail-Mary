70ac16f04429b1b47291fa85a6201f6146b6a521
Ethan <aurora314156@gmail.com>
2019-04-09 02:39:30 -0700

    debug done

--- BERTService/TFIDF.py
+++ BERTService/TFIDF.py
@@ -11,7 +11,7 @@ class TFIDF():
         self.stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
     def getTFIDFWeigths(self):
-        
+        print("start get TF-IDF weights\n")
         corpus = self.getListOfAllData()
         #将文本中的词语转换为词频矩阵
         vectorizer = CountVectorizer()
