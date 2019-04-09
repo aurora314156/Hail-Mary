1e6419a1752d43b1a245255877cae235f58ee650
Ethan <aurora314156@gmail.com>
2019-04-09 02:36:27 -0700

    debug

--- BERTService/TFIDF.py
+++ BERTService/TFIDF.py
@@ -67,7 +67,7 @@ class TFIDF():
                 tmp = tmp.lower().split(" ")
                 for t in tmp:
                     print(t)
-                    if t is " " or t[0] is None or t is "":
+                    if t is " " or t is None or t is "":
                         continue
                     elif t in self.stop_words:
                         continue
