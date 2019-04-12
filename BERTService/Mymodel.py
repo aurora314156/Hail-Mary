ffa6ea23539552a39b0da33b132416d5335aeb5d
aurora314156 <aurora314156@gmail.com>
2019-04-11 23:54:51 -0700

    EighteenthModel first test

--- BERTService/Mymodel.py
+++ BERTService/Mymodel.py
@@ -53,6 +53,8 @@ class Mymodel():
             guessAnswer = self.SixteenthModel(self.bc)
         if self.model == 'SeventeenthModel':
             guessAnswer = self.SeventeenthModel(self.bc)
+        if self.model == 'EighteenthModel':
+            guessAnswer = self.EighteenthModel(self.bc)
         if self.model == 'TestModel2':
             guessAnswer = self.TestModel(self.bc)
         
@@ -630,6 +632,53 @@ class Mymodel():
                 highestScore = tmpScore
             ind += 1
 
+        return guessAnswer
+    def EighteenthModel(self, bc):
+        """
+        encode eacht story sentences, then calculate similarity with question and options
+        choose highest scores options as final answer
+        """
+        sentences, tmp_string, sentence = [], "", ""
+        for s in self.s_string[:len(self.s_string)-1]:
+            tmp_string += s
+            # reserve sentence structure
+            if s == "." or s == "?" or s == "!":
+                # remove "," "." "?"
+                sentence = ""
+                for t in tmp_string:
+                    if t is "," or t is "." or t is "?":
+                        continue
+                    else:
+                        sentence += t
+                if len(sentence) >1:
+                    if sentence[0] == " ":
+                        sentences.append(sentence[:-1])
+                    else:
+                        sentences.append(sentence)
+                tmp_string = ""
+                continue
+
+        # use whole story structure
+        if tmp_string != "":
+            sentences.append(tmp_string)
+        
+        storySentences = self.activationFunction(bc.encode(sentences))
+        print(storySentences.shape)
+        for i in range(len(self.options)):
+            self.options[i] = self.options[i] + self.q_string
+        
+        merQueOpts = self.activationFunction(bc.encode(self.options))
+
+        ind, guessAnswer, highestScore, highestScore_storyVector = 0, 0, 0, []
+
+        for m in merQueOpts:
+            for s in storySentences:
+                tmpScore = 1 - spatial.distance.cosine(s, m)
+                if tmpScore > highestScore:
+                    guessAnswer = ind
+                    highestScore = tmpScore
+            ind += 1
+        
         return guessAnswer
     
 
