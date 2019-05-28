01971f3b7827ff8483c53204659db3249f2c6665
aurora314156 <aurora314156@gmail.com>
2019-05-28 00:58:28 -0700

    debug

--- BERTService_MOST/BERTmain.py
+++ BERTService_MOST/BERTmain.py
@@ -19,8 +19,10 @@ def randomNum(corpus_amount, flag):
 fraction = 1
 # lim_start = [6840, 0, 1710, 3420, 5130]
 # lim_end = [8549, 1709, 3419, 5129, 6839]
-lim_start = [7694, 0, 855, 1710, 2565, 3420, 4275, 5130, 5985, 6840]
-lim_end = [8549, 854, 1709, 2564, 3419, 4274, 5129, 5984, 6839, 7693]
+# lim_start = [7694, 0, 855, 1710, 2565, 3420, 4275, 5130, 5985, 6840]
+# lim_end = [8549, 854, 1709, 2564, 3419, 4274, 5129, 5984, 6839, 7693]
+lim_start = [10, 20]
+lim_end = [15, 25]
 def main():
     # initial dataset
     dataset, dataType, model = Initial().InitialMain()
@@ -77,7 +79,7 @@ def main():
                     dataTypeLog = "Data type: " + dataType[1] + "\n"
                 SaveLog(dataTypeLog, Process_dataset, model, Accuracy, CostTime).saveLogTxt()
                 dataTypeJson += 1
-            with open(m +'_wrongNum.json', 'w') as json_file:
+            with open('Experiment/'+ m +'_wrongNum.json', 'w') as json_file:
                 json.dump(wrongNumJson, json_file)
             constant += 0
     SaveLog(dataTypeLog, Process_dataset, model, Accuracy, CostTime, AccuracyList).saveLogExcel()
