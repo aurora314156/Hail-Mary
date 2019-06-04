2b38abf6824bf667b91ff901a4d5a8756b97e423
Ethan <aurora314156@gmail.com>
2019-06-04 08:42:50 -0700

    debug

--- extractData_MOST.py
+++ extractData_MOST.py
@@ -4,9 +4,24 @@ import os
 import json
 import re
 
-def removePunctuation(s):
-    string = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+「」——！，。？、~@#￥%……&*（）*()?]+", "", s)
-    return string
+# def removePunctuation(s):
+#     string = re.sub("[\s+\.\!\/_,$%^*()+\"\']+|[+「」『』….:——！，。？、~@#￥%……&*（）*()?=-]+", "", s)
+#     return string
+
+def removePunctuation(char):
+    char = re.sub("[\s+\.\!\/_,$%^*()+\"\']+|[+「」『』….:——！，。？、~@#￥%……&*（）*()?=-]+", "", char)
+    appendWord = ""
+    # reserve zh
+    #if '\u4e00' <= char <= '\u9fff' or '\u2e80'<= char <= '\u2fdf' or '\u3400'<= char <= '\u4dbf':
+    if char >= u'\u4E00' and char <= u'\u9FA5':
+       appendWord = char
+    # reserve number
+    elif char >= u'\u0030' and char <= u'\u0039':
+       appendWord = char
+    elif ( char >= u'\u0041' and char <= u'\u005A' ) or ( char >= u'\u0061' and char <= u'\u007A'):
+        appendWord = char
+    
+    return appendWord
 
 fileLen = [0, 1296, 1254, 1500, 1500, 1500, 1500]
 cut_punctuation = {"，", "。", "；", "？", "！", "："}
@@ -19,6 +34,7 @@ with open(fileName, 'w') as CQA:
         with open(str(r)+'.json', 'r') as a:
             correct_ans = json.loads(a.read())
             for fileLenInd in range(fileLen[r]):
+            #for fileLenInd in range(603,604):
                 fileName = str(fileLenInd+1) +".txt"
                 with open(os.path.join(type_path, fileName), 'r') as f:
                     content = {}
@@ -36,11 +52,13 @@ with open(fileName, 'w') as CQA:
                                         continue
                                     else:
                                         tmp_list.append(tmp_words)
+                                        #print(tmp_list)
                                     tmp_words = ""
                                 else:
                                     tmp_words += word
+                            #print(tmp_words)
                             if tmp_words is not "" and tmp_words is not "\n":
-                                tmp_list.append(tmp_words.replace("\n", ""))
+                                tmp_list.append(removePunctuation(tmp_words.replace("\n", "")))
                             content['story'] = tmp_list
                             tmp_list = []
                         # Q
